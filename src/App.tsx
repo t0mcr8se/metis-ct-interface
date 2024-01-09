e552764739fb137d39214c4f710fa9002c3933db
Rami Husami <husamirami@gmail.com>
2024-01-09 17:53:08 +0400

    move home page contents to Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
-import Header from './components/Header/Header.tsx'
-import SeasonOne from './components/SeasonOne/SeasonOne.tsx'
-import SeasonTwo from './components/SeasonTwo/SeasonTwo.tsx'
-import Ecosystem from './components/Ecosystem/Ecosystem.tsx'
-import Footer from './components/Footer/Footer.tsx'
-import './App.css'
+import Home from './pages/Home/index.tsx'
 
 function App() {
-
     return (
-        <>
-            <div className="season_one">
-                <Header />
-                <SeasonOne />
-            </div>
-            <Ecosystem />
-            <SeasonTwo />
-            <Footer />
-        </>
+        <Home />
     )
 }
-
-<style>
-    
-</style>
 
 export default App
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,21 @@
+import Header from '../../components/Header/Header.tsx'
+import SeasonOne from '../../components/SeasonOne/SeasonOne.tsx'
+import SeasonTwo from '../../components/SeasonTwo/SeasonTwo.tsx'
+import Ecosystem from '../../components/Ecosystem/Ecosystem.tsx'
+import Footer from '../../components/Footer/Footer.tsx'
+
+import './Home.css';
+
+export default function Home() {
+    return (
+        <>
+            <div className="season_one">
+                <Header />
+                <SeasonOne />
+            </div>
+            <Ecosystem />
+            <SeasonTwo />
+            <Footer />
+        </>
+    )
+}
