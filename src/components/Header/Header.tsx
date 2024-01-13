9e36df7d7e0bd53ab1090d8caa36abac77927bc5
Rami Husami <husamirami@gmail.com>
2024-01-13 12:14:38 +0400

    add scores

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,13 +4,14 @@ import navItems from '../../store/headerNavItems.json'
 
 import logo from '../../assets/images/bluemetis.svg'
 import logo_mob from '../../assets/images/logo_mob.svg'
-// import arrow_header from '../../assets/images/arrow_down.svg'
-import wallet_one from '../../assets/header_icons/wallet_one.svg'
-// import token_id from '../../assets/header_icons/wallet_three.svg'
-// import light_theme from '../../assets/header_icons/hover.svg'
 import xp_icon from '../../assets/header_icons/xp_icon.svg'
+import { useUser } from '../../hooks/useUser.ts'
+import { useAccount } from 'wagmi'
 
 const Header = () => {
+    
+    const { address } = useAccount()
+    const user = useUser(address)
     return (
         <>
             <div className={styles.header}>
@@ -28,7 +29,7 @@ const Header = () => {
                     </div>
                     <div className={styles.wallet_items}>
                         {/* <HeaderTokeInfo icon={wallet_one} type="wallet_block_one" text="36 Metis" /> */}
-                        <HeaderTokeInfo icon={xp_icon} type="wallet_block_two" text="142" />
+                        <HeaderTokeInfo icon={xp_icon} type="wallet_block_two" text={user?.score ?? 0} />
                         {/* <HeaderTokeInfo icon={token_id} type="user_wallet_address" text="0x7C01FB632424Ba62D02367EeD1CD8688D49A7a27" /> */}
                         <w3m-button />
                     </div>
--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,10 +1,13 @@
 import { useQuery } from "@apollo/client";
 import { PAIRS_QUEY, USER_QUERY } from "../api/apollo";
+import { useMemo } from "react";
 
-export function useUser (address: string) {
-    return useQuery(USER_QUERY, {variables: {address}})
+export function useUser (address?: string) {
+    const resp = useQuery(USER_QUERY, {variables: {address: address?.toLowerCase()}})
+    return useMemo(() => resp.data?.user, [resp])
 }
 
 export function usePairs () {
-    return useQuery(PAIRS_QUEY)
+    const resp = useQuery(PAIRS_QUEY)
+    return useMemo(() => resp.data?.pairs, [resp])
 }
