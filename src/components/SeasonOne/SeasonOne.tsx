26dc2a2f76566596b1a37b729bc4144d645634b7
Rami Husami <husamirami@gmail.com>
2024-01-15 18:22:30 +0400

    update szn1 message

--- src/components/SeasonOne/SeasonOne.tsx
+++ src/components/SeasonOne/SeasonOne.tsx
@@ -18,12 +18,17 @@ function SeasonOne() {
                     <div className={styles.main_info}>
                         <TitleOne text='Season One' titleType='white' />
                         <div className={styles.info_text}>
-                            <p>Community Testing will be a period where users will be able to contribute to the testing for our upcoming decentralized sequencer. It will be divided in three (2) Seasons: Season 1, and Season 2.</p>
                             <p>
-                            For Season 1, specifically, users will be able to explore dApps, existing and new, on our Sepolia testnet. By completing the different activities, users will earn points that will be claimable at the end of this testing period!
+                            During Community Testing, anyone will be able to contribute to our upcoming decentralized sequencer, without any coding experience needed! 
                             </p>
                             <p>
-                            This page will provide provide you everything you need to succeed 👇
+                            The activity will be divided in two (2) Seasons.
+                            </p>
+                            <p>
+                            For Season 1, specifically, users will be able to explore decentralized apps (DApps) on Sepolia testnet, and earn points by completing each of the actions!
+                            </p>
+                            <p>
+                            This page will provide provide you everything you need to succeed.
                             </p>
                         </div>
                     </div>
