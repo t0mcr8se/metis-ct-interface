9afba80dcacbecbb9f82213accc1c64453bf6fa8
electricddev <douwe@apedlabs.com>
2024-02-27 19:21:54 +0000

    add loading

--- src/hooks/useHelmetDrop.ts
+++ src/hooks/useHelmetDrop.ts
@@ -1,51 +1,57 @@
-import { useCallback, useEffect, useMemo, useState } from "react"
-import { HELMET_DROP_ADDRESS, HELMET_PROOF_URL, HELMET_SCOREBOARD_URL } from "../constants"
-import axios from "axios"
-import { useContractRead, useContractWrite } from "wagmi"
-import HELMET_DROP_ABI from '../abi/helmetDrop'
-
+import { useCallback, useEffect, useMemo, useState } from "react";
+import {
+  HELMET_DROP_ADDRESS,
+  HELMET_PROOF_URL,
+  HELMET_SCOREBOARD_URL,
+} from "../constants";
+import axios from "axios";
+import { useContractRead, useContractWrite } from "wagmi";
+import HELMET_DROP_ABI from "../abi/helmetDrop";
 
 export function useMerkleProof(address?: string) {
-    const [data, setData] = useState<undefined | any>(undefined)
-    useEffect(() => {
-      if(!address) return
-      const fetchData = async (url: string) => {
-        const result = await axios(url)
-        setData(result)
-      }
-      fetchData(HELMET_PROOF_URL+address)
-    }, [address])
+  const [data, setData] = useState<undefined | any>(undefined);
+  useEffect(() => {
+    if (!address) return;
+    const fetchData = async (url: string) => {
+      const result = await axios(url);
+      setData(result);
+    };
+    fetchData(HELMET_PROOF_URL + address);
+  }, [address]);
 
-    return useMemo(() => {
-      if (!data) return { loading: true };
-      if (data.status !== 200) return {found: false}
-      const {leafIndex, helmetType, points, ranking, joinId, proof} = data.data;
-      return {
-        found: true,
-        leafIndex,
-        helmetType,
-        points,
-        ranking,
-        joinId,
-        proof
-      };
-    }, [data]); 
+  return useMemo(() => {
+    if (!data) return { loading: true };
+    if (data.status !== 200) return { found: false };
+    const { leafIndex, helmetType, points, ranking, joinId, proof } = data.data;
+    return {
+      found: true,
+      leafIndex,
+      helmetType,
+      points,
+      ranking,
+      joinId,
+      proof,
+    };
+  }, [data]);
 }
 
-export function useFetchScoreboardPage(page:number = 0, category: string) {
-    const [data, setData] = useState<undefined | any>(undefined)
-    useEffect(() => {
-      const fetchData = async (url: string) => {
-        const result = await axios(url)
-        setData(result)
-      }
-      fetchData(HELMET_SCOREBOARD_URL+page+`&?helmetType=${category}`)
-    }, [page, category])
-    return useMemo(() => {
-      if (!data) return { loading: true, scoreboard: [] };
-      if (data.status !== 200) return {found: false, scoreboard: []}
-      return {scoreboard: data.data, found: true}
-    }, [data]); 
+export function useFetchScoreboardPage(page: number = 0, category: string) {
+  const [data, setData] = useState<undefined | any>(undefined);
+
+  useEffect(() => {
+    const fetchData = async (url: string) => {
+      setData(undefined);
+
+      const result = await axios(url);
+      setData(result);
+    };
+    fetchData(HELMET_SCOREBOARD_URL + page + `&helmetType=${category}`);
+  }, [page, category]);
+  return useMemo(() => {
+    if (!data) return { loading: true, scoreboard: [] };
+    if (data.status !== 200) return { found: false, scoreboard: [] };
+    return { scoreboard: data.data, found: true };
+  }, [data]);
 }
 
 const helmetDropContract: any = {
@@ -66,7 +72,8 @@ export function useClaimHelmet(address?: string) {
     () => hasClaimedData && (hasClaimedData as any),
     [hasClaimedData]
   );
-  const {points, helmetType, joinId, ranking, proof, found, loading} = useMerkleProof(address)
+  const { points, helmetType, joinId, ranking, proof, found, loading } =
+    useMerkleProof(address);
 
   const {
     write: claimCall,
@@ -94,6 +101,6 @@ export function useClaimHelmet(address?: string) {
     ranking,
     found,
     loading,
-    joinId
-  }
+    joinId,
+  };
 }
