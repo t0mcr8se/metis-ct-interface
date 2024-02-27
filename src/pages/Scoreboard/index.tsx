d40e567e141b0535254e9dc6659b4458ebf82115
Rami Husami <husamirami@gmail.com>
2024-02-28 02:10:45 +0800

    prettify category selection

--- src/pages/Scoreboard/index.tsx
+++ src/pages/Scoreboard/index.tsx
@@ -5,8 +5,8 @@ import { ScrollRestoration } from "react-router-dom";
 export default function Scoreboard() {
   const [selectedCategory, setCategory] = useState("1")
 
-  const handleCategoryChange = (event: any) => {
-    setCategory(event.target.value);
+  const handleCategoryClick = (value: string) => {
+    setCategory(value);
   };
 
   return (
@@ -17,18 +17,30 @@ export default function Scoreboard() {
           <label htmlFor="category" className="text-white font-raleway text-lg">
             Select Category:
           </label>
-          <select
-            id="category"
-            name="category"
-            value={selectedCategory}
-            onChange={handleCategoryChange}
+          <button
+            className={`px-4 py-2 rounded-full text-white font-raleway text-lg hover:bg-gray-200 ${
+              selectedCategory === "1" ? "bg-gray-600" : ""
+            }`}
+            onClick={() => handleCategoryClick("1")}
           >
-            {/* Add options for different categories */}
-            <option value="1">ELITE</option>
-            <option value="2">BASED</option>
-            <option value="3">GOOD</option>
-            {/* Add more options as needed */}
-          </select>
+            ELITE
+          </button>
+          <button
+            className={`px-4 py-2 rounded-full text-white font-raleway text-lg hover:bg-gray-200 ${
+              selectedCategory === "2" ? "bg-gray-600" : ""
+            }`}
+            onClick={() => handleCategoryClick("2")}
+          >
+            BASED
+          </button>
+          <button
+            className={`px-4 py-2 rounded-full text-white font-raleway text-lg hover:bg-gray-200 ${
+              selectedCategory === "3" ? "bg-gray-600" : ""
+            }`}
+            onClick={() => handleCategoryClick("3")}
+          >
+            GOOD
+          </button>
         </div>
         <h1 className="text-white font-raleway text-7xl text-center tracking-wide">
           Scoreboard
